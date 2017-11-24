5cc0e65df473dc2334d03593b28b3fcab14380a0
Alexey Samoshkin <al.samoshkin@gmail.com>
2017-11-24 23:58:55 +0200

    Configure keybinding and command to renew environment (SSH_AUTH_SOCK, DISPLAY) inside tmux session when attached back to existing session

---
+++ tmux/renew_env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -eu
+
+pane_fmt="#{pane_id} #{pane_in_mode} #{pane_input_off} #{pane_dead} #{pane_current_command}"
+tmux list-panes -s -F "$pane_fmt" | awk '
+  $2 == 0 && $3 == 0 && $4 == 0 && $5 ~ /(bash|zsh|ksh|fish)/ { print $1 }
+' | while read -r pane_id; do
+  # renew environment variables according to update-environment tmux option
+  # also clear screen
+  tmux send-keys -t "$pane_id" 'Enter' 'eval "$(tmux show-env -s)"' 'Enter' 'C-l'
+done;
