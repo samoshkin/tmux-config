023c751d5677fd42379f69df9301ea0e50d22776
Frank Perrakis <frank.perrakis@gmail.com>
2022-01-07 14:04:51 +0000

    added tmux network in status line , tmux addons :resurrect , suspend

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ fi
 if [ ! -e "$HOME/.tmux/plugins/tpm" ]; then
   printf "WARNING: Cannot found TPM (Tmux Plugin Manager) \
  at default location: \$HOME/.tmux/plugins/tpm.\n"
-  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+  git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm
 fi
 
 if [ -e "$HOME/.tmux.conf" ]; then
@@ -36,7 +36,7 @@ ln -sf .tmux/tmux.conf "$HOME"/.tmux.conf;
 # create dump __noop session in detached mode, and kill it when plugins are installed
 printf "Install TPM plugins\n"
 tmux new -d -s __noop >/dev/null 2>&1 || true 
-tmux set-environment -g TMUX_PLUGIN_MANAGER_PATH "~/.tmux/plugins"
+tmux set-environment -g TMUX_PLUGIN_MANAGER_PATH "$HOME/.tmux/plugins"
 "$HOME"/.tmux/plugins/tpm/bin/install_plugins || true
 tmux kill-session -t __noop >/dev/null 2>&1 || true
 
