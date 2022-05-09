cdb6e864c96eb0553151dc0d17d246c6261105d3
Armin Veres <armin.veres@hotmail.com>
2022-05-09 15:22:16 +0200

    rewrote to have a working XDG_CONFIG location

--- install.sh
+++ install.sh
@@ -17,10 +17,10 @@ Install it first\n"
   exit 1
 fi
 
-if [ ! -e "$HOME/.tmux/plugins/tpm" ]; then
+if [ ! -e "$HOME/.config/tmux/plugins/tpm" ]; then
   printf "WARNING: Cannot found TPM (Tmux Plugin Manager) \
- at default location: \$HOME/.tmux/plugins/tpm.\n"
-  git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+ at default location: \$HOME/config/tmux/plugins/tpm.\n"
+  git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 fi
 
 if [ -e "$HOME/.tmux.conf" ]; then
@@ -28,16 +28,18 @@ if [ -e "$HOME/.tmux.conf" ]; then
 fi
 
 cp -f "$HOME/.tmux.conf" "$HOME/.tmux.conf.bak" 2>/dev/null || true
-cp -a ./tmux/. "$HOME"/.tmux/
-ln -sf .tmux/tmux.conf "$HOME"/.tmux.conf;
+
+cp -a ./tmux/. "$HOME"/.config/tmux/
+# ln -sf .tmux/tmux.conf "$HOME"/.tmux.conf;
 
 # Install TPM plugins.
 # TPM requires running tmux server, as soon as `tmux start-server` does not work
 # create dump __noop session in detached mode, and kill it when plugins are installed
 printf "Install TPM plugins\n"
 tmux new -d -s __noop >/dev/null 2>&1 || true 
-tmux set-environment -g TMUX_PLUGIN_MANAGER_PATH "~/.tmux/plugins"
-"$HOME"/.tmux/plugins/tpm/bin/install_plugins || true
+tmux set-environment -g TMUX_PLUGIN_MANAGER_PATH "~/.config/tmux/plugins"
+"$HOME"/.config/tmux/plugins/tpm/bin/install_plugins || true
 tmux kill-session -t __noop >/dev/null 2>&1 || true
 
 printf "OK: Completed\n"
+printf "Please add an alias to TMUX: alias tmux='tmux -f ~/.config/tmux/tmux.conf'"
